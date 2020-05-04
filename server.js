eb180a04558472e15fbbe36bc83fc64352886e75
namhd1992 <stpnghd.vn@gmail.com>
2020-05-04 09:54:12 +0700

    fix port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3006;
+const port = process.env.PORT || 3007;
 const path = require('path');
 const fs = require('fs')
 
